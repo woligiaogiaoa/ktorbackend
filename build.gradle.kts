6b39ff5a3e4e1ecd1af781b658b3283832f1472c
admin <450121066@qq.com>
2021-06-03 20:26:52 +0800

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val exposed_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+}
